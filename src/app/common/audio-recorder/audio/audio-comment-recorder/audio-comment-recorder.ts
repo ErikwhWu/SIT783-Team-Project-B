45ed6c5f1be0041b3874d8adac35a35cdbf3aa2a
ErikwhWu <102471977+ErikwhWu@users.noreply.github.com>
2023-04-24 18:01:56 +1000

    Add files via upload

---
+++ src/app/common/audio-recorder/audio/audio-comment-recorder/audio-comment-recorder.ts
@@ -0,0 +1,59 @@
+import { Inject, Input, Component } from '@angular/core';
+import { BaseAudioRecorderComponent } from '../base-audio-recorder';
+import { audioRecorderService,  alertService } from 'src/app/ajs-upgraded-providers';
+import { TaskComment, TaskCommentService, Task } from 'src/app/api/models/doubtfire-model';
+
+@Component({ selector: 'audio-comment-recorder', templateUrl: './audio-comment-recorder.html' })
+export class AudioCommentRecorderComponent extends BaseAudioRecorderComponent {
+  @Input() task: Task;
+  canvas: HTMLCanvasElement;
+  canvasCtx: CanvasRenderingContext2D;
+  isSending: boolean;
+
+  constructor(
+    @Inject(audioRecorderService) mediaRecorderService: any,
+    @Inject(alertService) private alerts: any,
+    private ts: TaskCommentService,
+  ) {
+    super(mediaRecorderService);
+  }
+  ngOnInit() {
+    if (this.canRecord) {
+      this.init();
+    }
+  }
+
+  init(): void {
+    super.init();
+    this.canvas = document.getElementById('audio-recorder-visualiser') as HTMLCanvasElement;
+    this.audio = document.getElementById('audioPlayer') as HTMLAudioElement;
+    this.canvasCtx = this.canvas.getContext('2d');
+  }
+
+  sendRecording(): void {
+    this.isSending = true;
+    if (this.blob && this.blob.size > 0) {
+      this.ts.addComment(this.task, this.blob, 'audio').subscribe(
+        (comment: TaskComment) => {
+          this.isSending = false;
+          this.scrollCommentsDown();
+        },
+        (failure: { data: { error: any } }) => {
+          this.alerts.add('danger', `Failed to post audio. ${failure.data != null ? failure.data.error : undefined}`);
+          this.isSending = false;
+        }
+      );
+
+      this.blob = {} as Blob;
+      this.recordingAvailable = false;
+    }
+  }
+
+  private scrollCommentsDown(): void {
+    setTimeout(() => {
+      const objDiv = document.querySelector('div.comments-body');
+      // let wrappedResult = angular.element(objDiv);
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }, 50);
+  }
+}
