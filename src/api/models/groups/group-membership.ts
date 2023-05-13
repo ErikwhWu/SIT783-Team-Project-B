31c4596d652b09f0b394887bfdf31bf1034931dd
ErikwhWu <102471977+ErikwhWu@users.noreply.github.com>
2023-05-13 23:47:28 +1000

    Delete src/api directory

--- src/api/models/groups/group-membership.ts
+++
@@ -1,9 +0,0 @@
-import { Entity } from 'ngx-entity-service';
-
-export class GroupMembership extends Entity {
-
-  public get student_name(): string {
-    console.log("implement student_name");
-    return "TODO NAME";
-  }
-}
--- src/api/models/tutorial-enrolment.ts
+++
@@ -1,7 +0,0 @@
-import { Entity } from 'ngx-entity-service';
-import { Tutorial, User } from './doubtfire-model';
-
-
-export class TutorialEnrolment extends Entity {
-  public tutorial: Tutorial;
-}
